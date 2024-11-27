aa24ac2432ea2af4b308ed4c2bbac2561a871bf1
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-11-27 17:45:41 -0300

    package app

--- src/main/java/com/reliquiasdamagia/api_rm/controller/CategoryController.java
+++ src/main/java/com/reliquiasdamagia/api_rm/controller/CategoryController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/category")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class CategoryController {
 
     private final CategoryService categoryService;
