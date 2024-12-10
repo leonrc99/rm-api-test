e34cb1c3eb8c73bc94aa72e681eb9f95057a22fa
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-12-10 17:30:42 -0300

    updated securityConfig

--- src/main/java/com/reliquiasdamagia/api_rm/security/SecurityConfig.java
+++ src/main/java/com/reliquiasdamagia/api_rm/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,6 +30,7 @@ public class SecurityConfig {
         http
                 .addFilterBefore(corsFilter(), ChannelProcessingFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         // Auth
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
